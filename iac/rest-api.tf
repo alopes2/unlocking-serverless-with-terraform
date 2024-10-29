056339d1135d6e4356a4052692aebe74e01daa29
Andre <andrevitorlopes@gmail.com>
2024-10-29 14:20:43 +0100

    Try / for method path

--- iac/rest-api.tf
+++ iac/rest-api.tf
@@ -52,7 +52,7 @@ resource "aws_lambda_permission" "apigw_lambda" {
 resource "aws_api_gateway_method_settings" "method_settings" {
   rest_api_id = aws_api_gateway_rest_api.api.id
   stage_name  = aws_api_gateway_stage.live.stage_name
-  method_path = "/${aws_api_gateway_method.method.http_method}"
+  method_path = "//${aws_api_gateway_method.method.http_method}"
 
   settings {
     throttling_burst_limit = 1
