004f3f9aba3cf876501363f0de5602a250794e67
Andre Lopes <andrevitorlopes@gmail.com>
2025-09-04 14:35:39 +0200

    Email

--- iac/iam-policies.tf
+++ iac/iam-policies.tf
@@ -25,3 +25,48 @@ data "aws_iam_policy_document" "create_movie_item" {
     ]
   }
 }
+
+data "aws_iam_policy_document" "publish_to_movies_updates_sns_topic" {
+  statement {
+    effect = "Allow"
+
+    actions = [
+      "sns:Publish",
+    ]
+
+    resources = [
+      aws_sns_topic.movie_updates.arn
+    ]
+  }
+}
+
+data "aws_iam_policy_document" "pull_message_from_sqs" {
+  statement {
+    effect = "Allow"
+
+    actions = [
+      "sqs:ReceiveMessage",
+      "sqs:DeleteMessage",
+      "sqs:GetQueueAttributes",
+    ]
+
+    resources = [
+      aws_sqs_queue.movie_updates_queue.arn
+    ]
+  }
+}
+
+data "aws_iam_policy_document" "email_notification" {
+  statement {
+    effect = "Allow"
+
+    actions = [
+      "ses:SendEmail",
+    ]
+
+    resources = [
+      aws_sesv2_email_identity.email_identity.arn,
+      aws_sesv2_configuration_set.configuration_set.arn,
+    ]
+  }
+}
--- iac/lambdas.tf
+++ iac/lambdas.tf
@@ -3,15 +3,38 @@ module "get_movie_lambda" {
   name     = "get-movie"
   policies = [data.aws_iam_policy_document.get_movie_item.json]
   environment_variables = {
-    TABLE_NAME = aws_dynamodb_table.movies.name
   }
 }
 
 module "create_movie_lambda" {
-  source   = "./modules/lambda"
-  name     = "create-movie"
-  policies = [data.aws_iam_policy_document.create_movie_item.json]
+  source = "./modules/lambda"
+  name   = "create-movie"
+  policies = [
+    data.aws_iam_policy_document.create_movie_item.json,
+    data.aws_iam_policy_document.publish_to_movies_updates_sns_topic.json
+  ]
+
   environment_variables = {
-    TABLE_NAME = aws_dynamodb_table.movies.name
+    TABLE_NAME    = aws_dynamodb_table.movies.name
+    SNS_TOPIC_ARN = "arn:aws:sns:${var.region}:${var.account_id}:${local.movies_update_topic_name}"
   }
+}
+
+module "email_notification_lambda" {
+  source = "./modules/lambda"
+  name   = "email-movie-notification"
+  policies = [
+    data.aws_iam_policy_document.pull_message_from_sqs.json,
+    data.aws_iam_policy_document.email_notification.json
+  ]
+  environment_variables = {
+    SOURCE_EMAIL      = "${var.email_identity}"
+    DESTINATION_EMAIL = "${var.destination_email}"
+  }
+}
+
+resource "aws_lambda_event_source_mapping" "email_notification_trigger" {
+  event_source_arn = aws_sqs_queue.movie_updates_queue.arn
+  function_name    = module.email_notification_lambda.arn
+  enabled          = true
 }
