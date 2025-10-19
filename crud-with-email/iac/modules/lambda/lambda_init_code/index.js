3c03001624a389976b80f329d1e89228a6b32f9b
Andre Lopes <andrevitorlopes@gmail.com>
2025-10-19 13:58:44 +0200

    Reset

--- crud-with-email/iac/modules/lambda/lambda_init_code/index.js
+++
@@ -1,9 +0,0 @@
-// Default handler generated in AWS
-exports.handler = async (event) => {
-  const response = {
-    statusCode: 200,
-    body: JSON.stringify('Hello from Lambda!'),
-  };
-
-  return response;
-};
