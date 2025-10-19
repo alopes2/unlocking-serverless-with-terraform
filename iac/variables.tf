bc71b391d2112efa3a698be8d5282235a4b41872
Andre Lopes <andrevitorlopes@gmail.com>
2025-10-19 14:14:25 +0200

    Add email notification

--- iac/variables.tf
+++ iac/variables.tf
@@ -8,3 +8,13 @@ variable "account_id" {
   description = "The ID of the default AWS account"
   type        = string
 }
+
+variable "email_identity" {
+  description = "The email to be configured in configured in SES"
+  type        = string
+}
+
+variable "destination_email" {
+  description = "The destination email for the email notification Lambda function"
+  type        = string
+}
